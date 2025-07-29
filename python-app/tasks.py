da4604d1bb9807769590ebfbfe49bb5cd547a775
libbyf <libby.frenkel@skai.io>
2025-07-29 14:26:30 +0300

    Add Basic Code

--- python-app/tasks.py
+++
@@ -1,6 +0,0 @@
-from invoke import task
-import pytest
-
-@task
-def test(c):
-    pytest.main(["tests/"])
--- python-app/tests/test_main.py
+++ python-app/tests/test_main.py
@@ -1,4 +1,6 @@
 from main import add
+import pytest
 
+@pytest.mark.test
 def test_add():
     assert add(2, 3) == 5
